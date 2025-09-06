8b86be2d75063d36641489d7d56614cb700ec035
ekcapaper <92367474+ekcapaper@users.noreply.github.com>
2025-09-06 22:05:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 from contextlib import asynccontextmanager, suppress
 from threading import Thread
 from typing import AsyncGenerator
@@ -12,13 +13,11 @@ log = logging.getLogger(__name__)
 
 redis_client = FakeAsyncRedis()
 
-async def data_main():
+async def raw_data_main():
     try:
         while True:
-            data = await redis_client.get("node_a")
-            data_int = int(data)
-            print(data)
-            await redis_client.set("node_a", data_int + 1)
+            raw_data = random.randint(0, 100)
+            await redis_client.set("raw_data", raw_data)
             await asyncio.sleep(1)
     except asyncio.CancelledError:
         log.info("data_main cancelled")
@@ -26,10 +25,23 @@ async def data_main():
     except Exception:
         log.exception("data_main crashed")
 
+# 전문가 시스템
+async def data_analysis():
+    try:
+        while True:
+            raw_data = await redis_client.get("raw_data")
+    except asyncio.CancelledError:
+        log.info("data_main cancelled")
+        raise
+    except Exception:
+        log.exception("data_main crashed")
+
+# 학습
+
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
     await redis_client.set("node_a", 0)
-    task = asyncio.create_task(data_main())
+    task = asyncio.create_task(raw_data_main())
     try:
         yield
     finally:
@@ -42,6 +54,10 @@ app = FastAPI(lifespan=lifespan)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/raw-data")
+async def raw_data():
+    return {"raw_data": int(redis_client.get("raw_data"))}
 
 if __name__ == '__main__':
     # uvicorn
